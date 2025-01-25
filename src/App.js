4b70c5316d532de56202647957a9d172832281bf
Matteo Errera <matteoerrera1@gmail.com>
2025-01-25 18:10:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Logo from "./Logo";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Logo className="App-logo" fill={"#ffffff"}/>
+        <p>
+          Stiamo arrivando.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
